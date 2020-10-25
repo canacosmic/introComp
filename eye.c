dfcd8fdccae56b9de390eed67c7cfafe1684a275
Roy C <61641010+canacosmic@users.noreply.github.com>
2020-10-25 01:49:32 -0500

    update
    Getting closer...i think...

--- eye.c
+++ eye.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+
 int main(){
 
  int n = 50;
- int i = 0;
- int j = 0;
- int a[n][n];
+ int i,j = 0;
+ int a[n][n]={};
 
  for (i=0; i<n; i++){
   for (j=0; j<n; j++){
-   if (a[i] == a[j]){
-    printf("1");
+   if (i == j){
+    printf("1\n");
    }else {
-    printf("0");
+    printf("0\n");
    }
   }
  }
- printf("/n");
+ printf("\n");
  return 0;
 }
