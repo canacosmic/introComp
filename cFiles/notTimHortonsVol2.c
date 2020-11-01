970c839951e40c528e1ce022b00d7aa2594b5bf6
Roy C <61641010+canacosmic@users.noreply.github.com>
2020-11-01 11:16:48 -0600

    style points

--- cFiles/notTimHortonsVol2.c
+++ cFiles/notTimHortonsVol2.c
@@ -29,98 +29,157 @@ int main(){
 
 
   /*** INICIA PROGRAMA ***/
-  printf("Escoga el producto deseado.Presione el botón A para café espresso, B para americano, C para capuccino o D para chocolate​.\n​");
+  printf("\n");
+  printf("===========================================================================================================================\n");
+  printf("\n");
+  printf("Escoga el producto deseado.Presione el botón: \n\nA para Café Espresso \n\nB para Americano \n\nC para Capuccino \n\nD para Chocolate​\n​");
   scanf("%c", &eleccion);
-
+  printf("\n");
+  printf("===========================================================================================================================\n");
   /* CASO ESPRESSO */
   if (eleccion == espresso){
 
    /* Cobro */
-   printf("El precio del espresso es de $15.\n");
+   printf("\n");
+   printf("*** El precio del espresso es de $15 pesos ***\n");
+   printf("\n");
    printf("Inserte una a una las monedas en la ranura.\n");
    scanf("%d", &dinero);
 
    while (dinero < precioEspresso){
     precioEspresso -= dinero;
-    printf("Inserta %d pesos más\n", precioEspresso);
+    printf("\n");
+    printf("Inserte %d pesos más\n", precioEspresso);
     scanf("%d", &dinero);
 
     if (dinero > precioEspresso){
      cambio = dinero - precioEspresso;
-     printf("Su cambio es de $%d pesos.\n", cambio);
+     printf("\n");
+     printf("--- Su cambio es de $%d pesos ---\n", cambio);
+     printf("\n");
+     printf("===========================================================================================================================\n");
     }
    }
    /* Preparación de ESPRESSO */
    aguaBank = aguaBank - 28;
    cafeBank = cafeBank -28;
    vasosBank = vasosBank - 1;
-   /* CASO AMERICANO */
+
+   /* Entrega */
+   printf("\n");
+   printf("Se ha entregado un Espresso.\nGracias por comprar en notTimHortons!\nQue tenga un buen día!\n");
+   printf("\n");
+   printf("===========================================================================================================================\n");
+
+   /*** CASO AMERICANO ***/
   } else if (eleccion == americano){
    /* Cobro */
-   printf("El precio de un café americano es de $12.\n");
+   printf("\n");
+   printf(" *** El precio de un Café Americano es de $12 ***\n");
+   printf("\n");
    printf("Inserte una a una las monedas en la ranura.\n");
    scanf("%d", &dinero);
 
    while (dinero < precioAmericano){
     precioAmericano -= dinero;
+    printf("\n");
     printf("Inserta %d pesos más\n", precioAmericano);
     scanf("%d", &dinero);
-
+    
     if (dinero > precioAmericano){
      cambio = dinero - precioAmericano;
-     printf("Su cambio es de $%d pesos.\n", cambio);
+     printf("\n");
+     printf(" --- Su cambio es de $%d pesos ---\n", cambio);
+     printf("\n");
+     printf("===========================================================================================================================\n");
     }
    }
-   /* Preparación de ESPRESSO */
+   /* Preparación de Americano*/
    aguaBank = aguaBank - 56;
    cafeBank = cafeBank - 20;
    vasosBank = vasosBank - 1;
+
+   /* Entrega */
+   printf("\n");
+   printf("Se ha entregado un Café Americano.\nGracias por comprar en notTimHortons!\nQue tenga un buen día!\n");
+   printf("\n");
+   printf("===========================================================================================================================\n");
+
    /* CASO CAPU */
   }else if (eleccion == capu){
    /* Cobro */
-   printf("El precio del capuccino es de $18.\n");
+   printf("\n");
+   printf("*** El precio del Capuccino es de $18 ***\n");
+   printf("\n");
    printf("Inserte una a una las monedas en la ranura.\n");
    scanf("%d", &dinero);
 
    while (dinero < precioCapu){
     precioCapu -= dinero;
+    printf("\n");
     printf("Inserta %d pesos más\n", precioCapu);
     scanf("%d", &dinero);
 
     if (dinero > precioCapu){
      cambio = dinero - precioCapu;
-     printf("Su cambio es de $%d pesos.\n", cambio);
+     printf("\n");
+     printf("--- Su cambio es de $%d pesos ---\n", cambio);
+     printf("\n");
+     printf("===========================================================================================================================\n");
     }
    }
-   /* Preparación de ESPRESSO */
+   /* Preparación de Capuccino */
    aguaBank = aguaBank - 56;
    cafeBank = cafeBank -20;
    lecheBank = lecheBank -25;
    vasosBank = vasosBank - 1;
+
+   /*Entrega*/
+   printf("\n");
+   printf("Se ha entregado un Capuccino.\nGracias por comprar en notTimHortons!\nQue tenga un buen día!\n");
+   printf("\n");
+   printf("===========================================================================================================================\n");
+
    /* CASO COCA */
   }else if (eleccion == cocoa){
    /* Cobro */
-   printf("El precio del chocolate caliente es de $10.\n");
+   printf("\n");
+   printf(" *** El precio del chocolate caliente es de $10 ***\n");
+   printf("\n");
    printf("Inserte una a una las monedas en la ranura.\n");
    scanf("%d", &dinero);
 
    while (dinero < precioCocoa){
     precioCocoa -= dinero;
+    printf("\n");
     printf("Inserta %d pesos más\n", precioCocoa);
     scanf("%d", &dinero);
 
     if (dinero > precioCocoa){
      cambio = dinero - precioCocoa;
-     printf("Su cambio es de $%d pesos.\n", cambio);
+     printf("\n");
+     printf("--- Su cambio es de $%d pesos ---\n", cambio);
+     printf("\n");
+    printf("===========================================================================================================================\n");
     }
    }
-   /* Preparación de ESPRESSO */
+   /* Preparación de Chocolate */
    aguaBank = aguaBank - 56;
    lecheBank = lecheBank - 25;
    chocoBank = chocoBank - 25;
    vasosBank = vasosBank - 1;
+
+   /*Entrega*/
+   printf("\n");
+   printf("Se ha entregado un Capuccino.\nGracias por comprar en notTimHortons!\nQue tenga un buen día!\n");
+   printf("\n");
+   printf("===========================================================================================================================\n");
+
   }else{
-   printf("Su eleccion no es valida. Intente de nuevo.\n");
+    printf("\n");
+    printf("Su eleccion no es valida. Intente de nuevo.\n");
+    printf("\n");
+    printf("===========================================================================================================================\n");
   }
  return 0;
 }
