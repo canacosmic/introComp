3b78a74fb9df0c8c0d55a6f22cf6590614086e7f
Roy C <61641010+canacosmic@users.noreply.github.com>
2020-11-26 08:31:45 -0600

    update

--- cFiles/canalizoR_201117_TicTacToe.c
+++ cFiles/canalizoR_201117_TicTacToe.c
@@ -1,61 +1,120 @@
 #include <stdio.h>
 
-/* Constantes Globales */
-const int o = 1;
-const int cruz = 2;
-const int borde = 3;
-const int vacio = 0;
+/* Variables Globales */
+char tablero[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
-int *tablero;
-
-/* Variables */
-int i;
-
-/* Convirtiendo coordenadas */
-const int convertirCoords[9] = [6,7,8,11,12,13,16,17,18]; 
+/* Inicializando funciones */
+void creandoTablero();
+void llenandoTablero();
+int checarGanador(int, char);
 
 
-/* inicializando nustro tablero ( OJO! void es el return type de la func, este caso no retorna nada) */
-void initTablero(int *tablero){
-
- for (i=0;i<25;i++){
-  /* poniedo todo el tablero como borde */
-  tablero[i]=borde;
- }
-
- for (i=0;i<9;i++){
-  /* poniendo en 0's las posiciones validas para jugar */
-  tablero[convertirCoords[i]]=vacio;
- }
-}
-
-/* Funcion para imprimir el tablero */
-void imprimirTablero(int *tablero){
- printf("\nTablero:\n");
- for(i=0;i<25;i++){
-  /* Imprimir nuevo rengón cada 5 posiciones */
-  if (i!=0 && i%5==0){
-   pritnf("\n");
-  }
-  printf("%d", tablero[i]);
- }
- pritnf("\n");
-}
 
 
 
 int main(){
 
- int tablero[25];
+ /* Variables locales */
+ int eleccionHumano;
+ int eleccionCompu;
+ char marcado;/* 'X' para humano 'O' para compu */
 
- /* iniciaizando el tablero */
- initTablero(&tablero[0]);
+ /* Ciclo while */
+   /* mientras que "checarGanador" retorne 2...*/
+ creandoTablero();
+ printf("Elige la casilla a marcar: ");
+ scanf("%d", &eleccionHumano);
+ llenandoTablero();/* con la eleccion del humano*/
+ /*eleccion compu */
+ /*Llenar tablero con eleccion compu*/
 
- 
- /* imprimir el tablero */
- imprimirTablero(&tablero[0]);
+ /* si "checarGanador" retorna 1...*/
+  /* printf("el gandor es: la compu/Humano\n")*/
 
-
+ /* si "checarGanador" retorna 0...*/
+  /*printf("Empate\n")*/
 
  return 0;
 }
+
+
+
+
+
+/*** Función: Creando el tablero ***/
+void creandoTablero(){
+ printf("\n\n");
+ printf("     |     |     \n");
+ printf(" %c   | %c   | %c   \n",tablero[1],tablero[2],tablero[3]);
+ printf("_____|_____|_____\n");
+ printf("     |     |     \n");
+ printf(" %c   | %c   | %c   \n",tablero[4],tablero[5],tablero[6]);
+ printf("_____|_____|_____\n");
+ printf("     |     |     \n");
+ printf(" %c   | %c   | %c   \n",tablero[7],tablero[8],tablero[9]);
+ printf("     |     |     \n");
+}
+
+/*** Función: Llenando el tablero ***/
+void llenandoTablero(int eleccion, char marcado){
+ /* Si se elige la casilla n y si esta disponible, marcar...*/
+ if (eleccion==1 && tablero[1]=='1'){
+  tablero[1] = marcado;
+ }else if (eleccion==2 && tablero[2]=='2'){
+  tablero[2] = marcado;
+ }else if (eleccion==3 && tablero[3]=='3'){
+  tablero[3] = marcado;
+ }else if (eleccion==4 && tablero[4]=='4'){
+  tablero[4] = marcado;
+ }else if (eleccion==5 && tablero[5]=='5'){
+  tablero[5] = marcado;
+ }else if (eleccion==6 && tablero[6]=='6'){
+  tablero[6] = marcado;
+ }else if (eleccion==7 && tablero[7]=='7'){
+  tablero[7] = marcado;
+ }else if (eleccion==8 && tablero[8]=='8'){
+  tablero[8] = marcado;
+ }else if (eleccion==9 && tablero[9]=='9'){
+  tablero[9] = marcado;
+ }else {
+  printf("Whoops! La casilla elejida no existe.\n")
+ }
+}
+
+/*** Función: Checar casillas ganadoras ***/
+
+
+   /* Return 1 para indicar partida ganada */
+   /* Return 0 para indicar que ya se llenó el tablero pero sin ganadores (empate) */
+   /* Return 2 para indicar que el juego sigue (aun hay casillas disponibles, seguir juego) */
+int checarGanador(){
+ /* Checando renglones */
+ if (tablero[1] == tablero[2] && tablero[2] == tablero[3]){
+  return 1;
+ }else if (tablero[4] == tablero[5] && tablero[5] == tablero[6]){
+  return 1;
+ }else if (tablero[7] == tablero[8] && tablero[8] == tablero[9]){
+  return 1;
+ 
+ /* Checando columnas */
+ }else if (tablero[1] == tablero[4] && tablero[4] == tablero[7]){
+  return 1;
+ }else if (tablero[2] == tablero[5] && tablero[5] == tablero[8]){
+  return 1;
+ }else if (tablero[3] == tablero[6] && tablero[6] == tablero[9]){
+  return 1;
+
+  /* Checando diagonales*/
+ }else if (tablero[1] == tablero[5] && tablero[5] == tablero[9]){
+  return 1;
+ }else if (tablero[3] == tablero[5] && tablero[5] == tablero[7]){
+  return 1;
+
+  /* Si ninguna coincide*/
+ }else if (tablero[1] != '1' && tablero[2] != '2' && tablero[3] != '3' && tablero[4] != '4' && tablero[5] != '5' && tablero[6] != '6' && tablero[7] != '7' && tablero[8] != '8' && tablero[9] != '9') {
+  return 0;
+ }else {
+  return 2; 
+ }
+}
+ /* Funcion donde la compu elije una casilla*/
---
+++ cFiles/canalizoR_201124_hungryAgent.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Variables globales*/
+
+/* inicializando las funciones */
+
+int main(){
+
+ /* variables locales */
+ int i,j;
+ char arena[20][20];
+ char agente = '@';
+ char comida = 'O';
+ char veneno = 'X';
+ 
+
+
+ return 0;
+}
+
+/* Funciones */
